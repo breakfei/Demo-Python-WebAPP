ea1382a5e70e5db78ce0578913183908a0505ed7
breakfei <357364395@qq.com>
2015-10-05 19:38:34 +0800

    add the default configuration file

---
+++ www/config_default.py
@@ -0,0 +1,18 @@
+'''
+default configuations
+'''
+__author__ = 'zhangshen'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3307,
+        'user': 'www',
+        'password': 'www',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
