dea315b49ae83d45562c144dc18c249ed77eb6eb
breakfei <357364395@qq.com>
2015-10-09 14:48:50 +0800

    add auth middleware

--- www/app.py
+++ www/app.py
@@ -9,6 +9,7 @@ from jinja2 import Environment, FileSystemLoader
 import www.ORM
 from www.coroweb import add_routes, add_static
 from www.config import configs
+from www.handlers import cookie2user, COOKIE_NAME
 
 def init_jinja2(app, **kw):
     logging.info('init jinja2...')
@@ -39,6 +40,23 @@ def logger_factory(app, handler):
         # yield from asyncio.sleep(0.3)
         return (yield from handler(request))
     return logger
+
+@asyncio.coroutine
+def auth_factory(app, handler):
+    @asyncio.coroutine
+    def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = yield from cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        if request.path.startwith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return (yield from handler(request))
+    return auth
 
 @asyncio.coroutine
 def data_factory(app, handler):
@@ -118,7 +136,7 @@ def index(request):
 def init(loop):
     yield from www.ORM.create_pool(loop=loop, **configs.db)
     app = web.Application(loop=loop, middlewares=[
-        logger_factory, response_factory
+        logger_factory, auth_factory, response_factory
     ])
     init_jinja2(app, filters= dict(datetime = datetime_filter))
     add_routes(app, 'handlers')
